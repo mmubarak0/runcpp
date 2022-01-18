79001fe9741fc895fe4f2bbfca5f7b7db240b938
mohammed mubarak fromandroid <modymu9@gmail.com>
2022-01-18 04:34:03 +0200

    fix .local/bin folder issue

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 #mkdir ~/.local/bin
 pip install termcolor
+x="$(pwd)"
+cd $Home && mkdir -p .local/bin/ && cd $x 
 chmod +x runcpp && cp -r runcpp $HOME/.local/bin/ && echo "Done this program was installed succesfully"
